22ed03a394e2136e10545cf6a29ad47b1dd38e18
Jay <53336235+johan-STYL@users.noreply.github.com>
2021-01-25 09:30:24 +0100

    githubpages build test

--- src/content.tsx
+++ src/content.tsx
@@ -33,7 +33,9 @@ const content: Content[] = [
 ]
 const defaultContent = {
   title: 'Hello',
-  desc: 'default: Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sunt, a voluptate eaque magnam iure?',
+  desc:
+    `As a Full Stack Engineer I've found a place where my love for tech and need for creativity can both live under the same roof. My maxim is to always develop my skills and I love trying new things. 
+    Although the tech I'm most familiar with is is React, HTML, CSS, JavaScript and Elm on the frontend and Express.js, MongoDB, Docker and AWS on the backend I have been dipping my toes in a few other ponds and I am confident in my ability to quickly get up to speed on most common technologies.`,
   path: '/'
 }
 export { content, defaultContent }
