14b04f718684d95107dcf3eb2692ddf1443830d5
johan-STYL <johan@styldesign.se>
2021-01-26 15:19:48 +0100

    update content.ts

--- src/content.tsx
+++ src/content.tsx
@@ -1,15 +1,6 @@
 
 const content: Content[] = [
   {
-    title: 'React',
-    markdown: `This app is built using react. As one of the frameworks I am most familiar with this was an easy choice for a single developer project.
-  
-  However I always strive to write reusable code and expect parts of this portfolio can be repurposed in future projects the same way some code in this project is based on a previous todo-app I've been developing for personal use. 
-  
-  link:[__ti-do__](https://tido.herokuapp.com)`,
-    path: '/react'
-  },
-  {
     title: 'content',
     markdown: `This page is generated from content in json and markdown format. I've found this to be a fast and simple way to manage content and it is but one small step away from fetching the content through an api. Good practice and in my opinion, easy to use.
 
@@ -24,6 +15,15 @@ To illustrate, here is an _example json-object._
 \`\`\`
     `,
     path: '/content'
+  },
+  {
+    title: 'React',
+    markdown: `This app is built using react. As one of the frameworks I am most familiar with this was an easy choice for a single developer project.
+  
+  However I always strive to write reusable code and expect parts of this portfolio can be repurposed in future projects the same way some code in this project is based on a previous todo-app I've been developing for personal use. 
+  
+  link:[__ti-do__](https://tido.herokuapp.com)`,
+    path: '/react'
   },
   {
     title: 'node',
@@ -62,7 +62,7 @@ const defaultContent = {
   title: 'Hello',
   markdown:
     `As a Full Stack Engineer I've found a place where my love for tech and need for creativity can both live under the same roof. My maxim is to always develop my skills and I love trying new things. 
-    Although the tech I'm most familiar with is is React, HTML, CSS, JavaScript and Elm on the frontend and Express.js, MongoDB, Docker and AWS on the backend I have been dipping my toes in a few other ponds and I am confident in my ability to quickly get up to speed on most common technologies.`,
+    Although the tech I'm most familiar with is is __React__, __Elm__, __HTML__, __CSS__ and of course __JavaScript/Typescript__ on the frontend and __Express.js__, __MongoDB__, __Docker__ and __AWS__ on the backend I have been dipping my toes in a few other ponds and I am confident in my ability to quickly get up to speed on most common technologies.`,
   path: '/'
 }
 export { content, defaultContent }
