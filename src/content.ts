4acfc145a4cceda564d0b8179aecef87418d7c28
johan-STYL <johan@styldesign.se>
2021-01-26 15:44:15 +0100

    tweek content

--- src/content.ts
+++ src/content.ts
@@ -26,7 +26,7 @@ This app is built using react. As one of the frameworks I am most familiar with 
 
 However I always strive to write reusable code and expect parts of this portfolio can be repurposed in future projects the same way some code in this project is based on a previous todo-app I've been developing for personal use. 
 
-~~link:[__ti-do__](https://tido.herokuapp.com)~~ _currently not live_`,
+~~link:[__ti-do__](https://tido.herokuapp.com)~~ _(currently not live)_`,
     path: '/react'
   },
   {
