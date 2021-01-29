5cbe459f6dbfdce31f42ebe3e1793c97dac94186
Jay <53336235+johan-STYL@users.noreply.github.com>
2021-01-29 16:03:34 +0100

    add default (404) route

--- src/components/Viewer/Viewer.tsx
+++ src/components/Viewer/Viewer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { Desc, Stack } from '..'
-import { defaultContent } from '../../content'
 
 type ViewerProps = {
   content: Content[]
@@ -12,13 +11,16 @@ const Viewer = (props: ViewerProps) => (
   <>
     <Switch>
       < Route exact path='/' render={
-        (rProps) => <Desc {...rProps} content={defaultContent} />
+        (rProps) => <Desc {...rProps} content={props.defaultContent} />
       } />
       {props.content.map(c =>
         <Route key={c.title + "_route"} exact path={c.path} render={
           (rProps) => <Desc {...rProps} content={c} />
         } />
       )}
+      <Route key={"404_route"} path="*" render={
+        (rProps) => <Desc {...rProps} content={props.defaultContent} />
+      } />
     </Switch>
     <Stack content={props.content} />
   </>
