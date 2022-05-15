740eefa1068cf17d119403b2e80096b5a92e1af7
johan-STYL <johan@styldesign.se>
2022-05-15 12:42:58 +0200

    modify email

--- src/components/Foot/Foot.tsx
+++ src/components/Foot/Foot.tsx
@@ -4,7 +4,7 @@ import './Foot.css';
 function Contact() {
   return (
     <div className="contact">
-      {" - "} <a href="mailto:johan.strand@appliedtechnology.se">johan.strand@appliedtechnology.se</a>
+      {" - "} <a href="mailto:jst@jst.dev">jst@jst.dev</a>
       {" - "} <a href="https://github.com/johan-st" target="_blank" rel="noopener noreferrer">GitHub</a>
       {" - "} <a href="https://www.linkedin.com/in/johan-strand-99a12b44/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       {" - "}
